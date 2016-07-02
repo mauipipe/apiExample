e50f84bec403ebb28a804a693bb1b9345761a5d3
davidcontavalli <david.contavalli@lovoo.com>
2016-07-02 15:58:08 +0200

    VM configuration fixed

--- resources/bootstrap_fixtures.sql
+++ resources/bootstrap_fixtures.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE `address`;
 
+USE 'address';
+
 CREATE USER 'user'@'localhost'
   IDENTIFIED BY '123456';
 
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 
 # Use single quotes instead of double quotes to make it work with special-character passwords
-PASSWORD='12345678'
+PASSWORD="12345678"
 
-# update / upgrade
+echo update / upgrade
 sudo apt-get update
 sudo apt-get -y upgrade
 
-#  php 5.5
-sudo apt-get install -y php5 nginx php5-fpm php5-cli
+echo  "installing php 5.5"
+sudo apt-get install -y php5 nginx php5-fpm php5-cli > /dev/null
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
 
-# preparing swap
+echo  "preparing swap"
 sudo dd if=/dev/zero of=/swapfile bs=1024 count=512k
 mkswap /swapfile
 swapon /swapfile
 
+echo "installing nginx"
 VHOST=$(cat <<EOF
 server {
     server_name localhost;
     root /var/www/address/app;
 
     location / {
-        # try to serve file directly, fallback to app.php
-        try_files $uri /index.php$is_args$args;
+        try_files \$uri /index.php\$is_args$args;
     }
 
     location ~ ^/index\.php(/|$) {
@@ -36,8 +38,8 @@ server {
         # Otherwise, PHP's OPcache may not properly detect changes to
         # your PHP files (see https://github.com/zendtech/ZendOptimizerPlus/issues/126
         # for more information).
-        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
-        fastcgi_param DOCUMENT_ROOT $realpath_root;
+        fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;
+        fastcgi_param DOCUMENT_ROOT \$realpath_root;
         # Prevents URIs that include the front controller. This will 404:
         # http://domain.tld/app.php/some-path
         # Remove the internal directive to allow URIs like this
@@ -50,8 +52,8 @@ server {
       return 404;
     }
 
-    error_log /var/log/nginx/project_error.log;
-    access_log /var/log/nginx/project_access.log;
+    error_log /var/log/nginx/address_error.log;
+    access_log /var/log/nginx/address_access.log;
 }
 EOF
 )
@@ -59,12 +61,42 @@ EOF
 echo "${VHOST}" > /etc/nginx/sites-available/address.local
 sudo ln -s /etc/nginx/sites-available/address.local /etc/nginx/sites-enabled/
 rm /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo service nginx restart > /dev/null
 
 
-# install mysql and give password to installer
+echo "install mysql and give password to installer"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server
-sudo apt-get install php5-mysql
+sudo apt-get -y install mysql-server php5-mysql > /dev/null
+
+echo "install mysql and give password to installer"
+file="/etc/mysql/my.cnf"
+
+if [ -w "$file" ] #Check if you have write permissions
+then
+ if ! grep -q collation-server "$file";
+ then
+   echo "Setting default collation to UTF-8 in /etc/mysql/my.cnf"
+  sed '/\[mysqld\]/a # set default collation\
+collation-server = utf8_unicode_ci\
+init-connect='\''SET NAMES utf8'\''\
+character-set-server = utf8\' -i "$file"
+sed 's/\\//g' -i "$file"
+ fi
+
+ if ! grep -q 'user = root' "$file" && ! grep -q 'password = 12345678' "$file";
+ then
+   echo "Setting user and password /etc/mysql/my.cnf"
+   sed '/\[mysql\]/a # set root password\
+user = root\
+password = 12345678\' -i "$file"
+sed 's/\\//g' -i "$file"
+  fi
+else
+  echo "Can't write to $file. ry running the script with 'sudo'."
+fi
+
+sudo service mysql restart
+
+cat /var/www/address/resources/bootstrap_fixtures.sql | mysql
 
